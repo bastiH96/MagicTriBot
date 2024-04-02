d8e39c0684aa9dfc7a82a1be5cf699c952563ee9
Sebastian H <sebastianheyde@t-online.de>
2024-04-02 04:44:33 +0200

    blank bot is ready to run on the server

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,7 +1,36 @@
-﻿namespace DiscordBot {
+﻿using DiscordBot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+
+namespace DiscordBot {
     internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Command { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JsonReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
--- DiscordBot/config/JsonReader.cs
+++ DiscordBot/config/JsonReader.cs
@@ -1,6 +1,26 @@
+using Newtonsoft.Json;
+
 namespace DiscordBot.config;
 
 public class JsonReader
 {
-    
+    public string Token { get; set; }
+    public string Prefix { get; set; }
+
+    public async Task ReadJson()
+    {
+        using (var sr = new StreamReader("config.json"))
+        {
+            string json = await sr.ReadToEndAsync();
+            JsonStructure data = JsonConvert.DeserializeObject<JsonStructure>(json);
+
+            this.Token = data!.Token;
+            this.Prefix = data.Prefix;
+        }
+    }
+}
+internal sealed class JsonStructure
+{
+    public string Token { get; set; }
+    public string Prefix { get; set; }
 }
